69e2099b29b87f3258eaa7b5b5a5333ed7b9f07b
GHL <gustavhl@hotmail.com>
2025-03-25 15:22:18 +0100

    nya grejer

--- gustav.py
+++ gustav.py
@@ -1,1 +1,2 @@
-print('gustav')
+def pizza():
+    print('gustav')
--- main.py
+++ main.py
@@ -1,31 +1,9 @@
-def like_text():
-   like()
+from gustav import pizza
 
-def ask_user_input(text):
-   ask = input(text)
-
-def like():
-   """Nåt är fel här"""
-   ask_user_input("Skriv nåt: ")
-   like_or_dislike = ask_user_input("Gillar du vad du har skrivit? ja/nej ")
-   if like_or_dislike == "ja":
-         print("Bra!")
-   elif like_or_dislike == "nej":
-      print("Oj då, skriv nåt nytt!")
-   else:
-      print("Välj ett giltig val!")
-
-like_text()
-
-def lol():
-   print("Do you like League of Legends?")
-   question = input("Answer with Yes or No: ")
-
-   if question == "Yes":
-      print("No you don't, you liar")
-   elif question == "No":
-      print("Good, you have a brain")
-   else:
-      print("The answer is no")
-   
-lol()
+def tres():
+    nummer = int(input('skriv ett nummer')) 
+    sum = 3 + nummer
+    print(f'3+{nummer} ='f' {sum}')
+    pizza()
+    
+tres()
