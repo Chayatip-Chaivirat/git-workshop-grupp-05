df8902648fe7afb1bd0e426aa1923c9fa7170c85
Leonard Werder <leonard.werder@gmail.com>
2025-03-25 15:01:59 +0100

    No

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def ask_user_input():
-   ask = input("Skriv nåt: ")
-   print(ask)
 
-ask_user_input()
+def lol():
+   print("Do you like League of Legends?")
+   question = input("Answer with Yes or No: ")
+
+   if question == "Yes":
+      print("No you don't, you liar")
+   elif question == "No":
+      print("Good, you have a brain")
+   else:
+      print("The answer is no")
+   
+lol()
+   
