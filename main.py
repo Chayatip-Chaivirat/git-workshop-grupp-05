7328c222a3dcaa8021ddc9899fba4059acdb45ef
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2025-03-25 15:07:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-def ask_user_input():
-   ask = input("Skriv nåt: ")
-   print(ask)
+def main():
+   like()
 
-ask_user_input()
+def ask_user_input(text):
+   ask = input(text)
+
+def like():
+   """Nåt är fel här"""
+   ask_user_input("Skriv nåt: ")
+   like_or_dislike = ask_user_input("Gillar du vad du har skrivit? ja/nej ")
+   if like_or_dislike == "ja":
+         print("Bra!")
+   elif like_or_dislike == "nej":
+      print("Oj då, skriv nåt nytt!")
+   else:
+      print("Välj ett giltig val!")
+
+main()
