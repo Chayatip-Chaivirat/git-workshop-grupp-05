761264f722c603e924d9d10fa2a99ac4275f7366
GoblinBuilds <ohrocky@live.com>
2025-03-25 15:37:46 +0100

    A goblin says hi.
    for the main.py file

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from gustav import pizza
-
+from hej_goblin2 import goblin_salute
 def tres():
     nummer = int(input('skriv ett nummer')) 
     sum = 3 + nummer
     print(f'3+{nummer} ='f' {sum}')
     pizza()
-    
+    goblin_salute()
 tres()
 
