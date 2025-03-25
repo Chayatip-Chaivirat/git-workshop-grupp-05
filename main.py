d15e271212c9b8232d4d5b52152fb04e5b681c54
GHL <gustavhl@hotmail.com>
2025-03-25 14:55:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from gustav import pizza
 
 def tres():
     nummer = input('skriv ett nummer')
-    sum = 3 + nummer
+    sum = 3 + int(nummer)
     print('3 + ditt nummer = '+ sum)
     pizza
 tres()
